88163e354e390a093a3c588778e2494f6b68e0d6
kyleredilla <kmredilla@alaska.edu>
2022-08-04 16:55:14 -0800

    initial commit, with list of filepath-based model names

---
+++ luts.py
@@ -0,0 +1,12 @@
+models = [
+    "ACCESS1-3",
+    "CanESM2",
+    "CCSM4",
+    "CSIRO-Mk3-6-0",
+    "GFDL-ESM2M",
+    "HadGEM2-ES",
+    "inmcm4",
+    "MIROC5",
+    "MPI-ESM-MR",
+    "MRI-CGCM3",
+]
