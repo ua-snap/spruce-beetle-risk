1ebb79f3be3a2bb416a0072540abff22af592bf3
kyleredilla <kmredilla@alaska.edu>
2022-08-10 15:35:26 -0800

    define historical era as 1990-2020 (for now)

--- luts.py
+++ luts.py
@@ -1,17 +1,10 @@
 models = [
-    "ACCESS1-3",
-    "CanESM2",
     "CCSM4",
-    "CSIRO-Mk3-6-0",
     "GFDL-ESM2M",
     "HadGEM2-ES",
-    "inmcm4",
-    "MIROC5",
-    "MPI-ESM-MR",
     "MRI-CGCM3",
 ]
 
 scenarios = ["rcp45", "rcp85"]
-# scenarios = ["hist", "rcp45", "rcp85"]
 
-eras = ["1950-2009", "2040-2069", "2070-2099"]
+eras = ["1990-2020", "2040-2099"]
