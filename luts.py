3aabf2b922212d384822fc9189d8f8c52da52426
kyleredilla <kmredilla@alaska.edu>
2022-09-16 19:52:59 -0800

    pull out lengthy setup code into config file

--- luts.py
+++ luts.py
@@ -8,3 +8,8 @@ models = [
 scenarios = ["rcp45", "rcp85"]
 
 eras = ["2010-2039", "2040-2069", "2060-2099"]
+
+# all projections will have years 2010-2099
+# need to start with 2008 as yearly risk calculation
+#   requires risk components from two years prior
+full_future_era = "2008-2099"
